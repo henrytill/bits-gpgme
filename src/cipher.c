44f8ffd6280dadded6625ffdcc05702e9c33bbd1
Henry Till <henrytill@gmail.com>
2022-08-25 14:22:12 -0700

    tweak

--- src/cipher.c
+++ src/cipher.c
@@ -8,14 +8,14 @@
 
 #include "cipher.h"
 
-#define FAILINIT    "could not initialize engine"
-#define FAILNEW     "could not create context"
-#define FAILHOME    "could not set homedir"
-#define FAILKEY     "could not fetch key"
-#define FAILINPUT   "could not create input data"
-#define FAILOUTPUT  "could not create output data"
-#define FAILENCRYPT "could not encrypt"
-#define FAILDECRYPT "could not decrypt"
+#define FAILINIT    "failed to initialize engine"
+#define FAILNEW     "failed to create context"
+#define FAILHOME    "failed to set homedir"
+#define FAILKEY     "failed to fetch key"
+#define FAILINPUT   "failed to create input data"
+#define FAILOUTPUT  "failed to create output data"
+#define FAILENCRYPT "failed to encrypt"
+#define FAILDECRYPT "failed to decrypt"
 
 enum {
   KEYSZ = 2,   /* NULL-terminated array of length 1 */
@@ -37,16 +37,14 @@ static gpgme_error_t init(gpgme_protocol_t proto) {
   setlocale(LC_ALL, "");
   gpgme_check_version(NULL);
   gpgme_set_locale(NULL, LC_CTYPE, setlocale(LC_CTYPE, NULL));
-
 #ifdef LC_MESSAGES
   gpgme_set_locale(NULL, LC_MESSAGES, setlocale(LC_MESSAGES, NULL));
 #endif
-
   return gpgme_engine_check_version(proto);
 }
 
 static void printerr(gpgme_error_t err, char *msg) {
-  fprintf(stderr, "%s: %s: %s\n", msg, gpgme_strsource((err)), gpgme_strerror((err)));
+  fprintf(stderr, "%s: %s: %s\n", msg, gpgme_strsource(err), gpgme_strerror(err));
 }
 
 #ifdef PRINT_KEY
@@ -72,10 +70,9 @@ static int writedata(gpgme_data_t data, FILE *fp) {
   gpgme_error_t err;
   char buf[BUFSZ + 1];
 
-  off = gpgme_data_seek(data, 0, SEEK_SET);
-  if (off != 0) {
+  if ((off = gpgme_data_seek(data, 0, SEEK_SET)) != 0) {
     err = gpgme_error_from_errno((int)off);
-    printerr(err, "could not seek");
+    printerr(err, "failed to seek");
     return FAILURE;
   }
 
@@ -84,7 +81,7 @@ static int writedata(gpgme_data_t data, FILE *fp) {
 
   if (off == -1) {
     err = gpgme_error_from_errno((int)off);
-    printerr(err, "could not read");
+    printerr(err, "failed to read");
     return FAILURE;
   }
 
@@ -98,28 +95,23 @@ int cipher_encrypt(const char *fgpt, const char *input, size_t inputsz, FILE *fp
   gpgme_key_t key[KEYSZ];
   gpgme_data_t in = NULL;
   gpgme_data_t out = NULL;
-  gpgme_encrypt_flags_t flags;
 
-  err = init(GPGME_PROTOCOL_OPENPGP);
-  if (err != SUCCESS) {
+  if ((err = init(GPGME_PROTOCOL_OPENPGP)) != SUCCESS) {
     printerr(err, FAILINIT);
     return FAILURE;
   }
 
-  err = gpgme_new(&ctx);
-  if (err != SUCCESS) {
+  if ((err = gpgme_new(&ctx)) != SUCCESS) {
     printerr(err, FAILNEW);
     return FAILURE;
   }
 
-  err = gpgme_ctx_set_engine_info(ctx, GPGME_PROTOCOL_OPENPGP, NULL, home);
-  if (err != SUCCESS) {
+  if ((err = gpgme_ctx_set_engine_info(ctx, GPGME_PROTOCOL_OPENPGP, NULL, home)) != SUCCESS) {
     printerr(err, FAILHOME);
     goto out0;
   }
 
-  err = gpgme_get_key(ctx, fgpt, &key[KEY], true);
-  if (err != SUCCESS) {
+  if ((err = gpgme_get_key(ctx, fgpt, &key[KEY], true)) != SUCCESS) {
     printerr(err, FAILKEY);
     goto out0;
   }
@@ -129,28 +121,23 @@ int cipher_encrypt(const char *fgpt, const char *input, size_t inputsz, FILE *fp
 
   gpgme_set_armor(ctx, true);
 
-  err = gpgme_data_new_from_mem(&in, input, inputsz, true);
-  if (err != SUCCESS) {
+  if ((err = gpgme_data_new_from_mem(&in, input, inputsz, true)) != SUCCESS) {
     printerr(err, FAILINPUT);
     goto out1;
   }
 
-  err = gpgme_data_new(&out);
-  if (err != SUCCESS) {
+  if ((err = gpgme_data_new(&out)) != SUCCESS) {
     printerr(err, FAILOUTPUT);
     goto out2;
   }
 
-  flags = GPGME_ENCRYPT_ALWAYS_TRUST;
-  err = gpgme_op_encrypt(ctx, key, flags, in, out);
-  if (err != SUCCESS) {
+  if ((err = gpgme_op_encrypt(ctx, key, GPGME_ENCRYPT_ALWAYS_TRUST, in, out)) != SUCCESS) {
     printerr(err, FAILENCRYPT);
     goto out3;
   }
 
-  if (writedata(out, fpout) != SUCCESS) {
+  if (writedata(out, fpout) != SUCCESS)
     goto out3;
-  }
 
   ret = SUCCESS;
 out3:
@@ -172,26 +159,22 @@ int cipher_decrypt(const char *fgpt, FILE *fpin, FILE *fpout, const char *home) 
   gpgme_data_t in = NULL;
   gpgme_data_t out = NULL;
 
-  err = init(GPGME_PROTOCOL_OPENPGP);
-  if (err != SUCCESS) {
+  if ((err = init(GPGME_PROTOCOL_OPENPGP)) != SUCCESS) {
     printerr(err, FAILINIT);
     return FAILURE;
   }
 
-  err = gpgme_new(&ctx);
-  if (err != SUCCESS) {
+  if ((err = gpgme_new(&ctx)) != SUCCESS) {
     printerr(err, FAILNEW);
     return FAILURE;
   }
 
-  err = gpgme_ctx_set_engine_info(ctx, GPGME_PROTOCOL_OPENPGP, NULL, home);
-  if (err != SUCCESS) {
+  if ((err = gpgme_ctx_set_engine_info(ctx, GPGME_PROTOCOL_OPENPGP, NULL, home)) != SUCCESS) {
     printerr(err, FAILNEW);
     goto out0;
   }
 
-  err = gpgme_get_key(ctx, fgpt, &key[KEY], true);
-  if (err != SUCCESS) {
+  if ((err = gpgme_get_key(ctx, fgpt, &key[KEY], true)) != SUCCESS) {
     printerr(err, FAILKEY);
     goto out0;
   }
@@ -199,20 +182,17 @@ int cipher_decrypt(const char *fgpt, FILE *fpin, FILE *fpout, const char *home) 
 
   printkey(key[KEY]);
 
-  err = gpgme_data_new_from_stream(&in, fpin);
-  if (err != SUCCESS) {
+  if ((err = gpgme_data_new_from_stream(&in, fpin)) != SUCCESS) {
     printerr(err, FAILINPUT);
     goto out1;
   }
 
-  err = gpgme_data_new(&out);
-  if (err != SUCCESS) {
+  if ((err = gpgme_data_new(&out)) != SUCCESS) {
     printerr(err, FAILOUTPUT);
     goto out2;
   }
 
-  err = gpgme_op_decrypt(ctx, in, out);
-  if (err != SUCCESS) {
+  if ((err = gpgme_op_decrypt(ctx, in, out)) != SUCCESS) {
     printerr(err, FAILDECRYPT);
     goto out3;
   }
--- test/roundtrip.c
+++ test/roundtrip.c
@@ -14,7 +14,6 @@ static const char *const CIPHERTEXT = "ciphertext.asc";
 static const char *const OUTPUT = "output.txt";
 
 int main(int argc, char *argv[]) {
-  int rc;
   FILE *ciphertext = NULL;
   FILE *output = NULL;
 
@@ -22,52 +21,44 @@ int main(int argc, char *argv[]) {
   (void)argv;
 
   const size_t inputsz = strlen(INPUT);
-
   char buf[inputsz + 1];
 
   {
     ciphertext = fopen(CIPHERTEXT, "wb");
     if (ciphertext == NULL) {
-      perror("Failed to open file");
+      perror("failed to open file");
       return EXIT_FAILURE;
     }
-
-    rc = cipher_encrypt(FINGERPRINT, INPUT, inputsz, ciphertext, GNUPGHOME);
-    if (rc != SUCCESS) {
-      perror("Failed to encrypt.");
+    if (cipher_encrypt(FINGERPRINT, INPUT, inputsz, ciphertext, GNUPGHOME) != SUCCESS) {
+      perror("failed to encrypt");
       fclose(ciphertext);
       remove(CIPHERTEXT);
       return EXIT_FAILURE;
     }
-
     fclose(ciphertext);
   }
 
   {
     ciphertext = fopen(CIPHERTEXT, "r");
     if (ciphertext == NULL) {
-      perror("Failed to open file");
+      perror("failed to open file");
       remove(CIPHERTEXT);
       return EXIT_FAILURE;
     }
-
     output = fopen(OUTPUT, "wb");
     if (output == NULL) {
-      perror("Failed to open file");
+      perror("failed to open file");
       fclose(ciphertext);
       remove(CIPHERTEXT);
       return EXIT_FAILURE;
     }
-
-    rc = cipher_decrypt(FINGERPRINT, ciphertext, output, GNUPGHOME);
-    if (rc != SUCCESS) {
-      perror("Failed to decrypt");
+    if (cipher_decrypt(FINGERPRINT, ciphertext, output, GNUPGHOME) != SUCCESS) {
+      perror("failed to decrypt");
       fclose(ciphertext);
       remove(CIPHERTEXT);
       fclose(output);
       remove(OUTPUT);
     }
-
     fclose(ciphertext);
     remove(CIPHERTEXT);
     fclose(output);
@@ -76,22 +67,16 @@ int main(int argc, char *argv[]) {
   {
     output = fopen(OUTPUT, "r");
     if (output == NULL) {
-      perror("Failed to open file");
+      perror("failed to open file");
       remove(OUTPUT);
       return EXIT_FAILURE;
     }
-
-    while (fgets(buf, (int)sizeof buf, output) != NULL) {
-      /* read */
-    }
-
+    while (fgets(buf, (int)sizeof buf, output) != NULL) continue;
     fclose(output);
     remove(OUTPUT);
   }
 
-  if (strcmp(INPUT, buf) != 0) {
-    return EXIT_FAILURE;
-  }
+  if (strcmp(INPUT, buf) != 0) return EXIT_FAILURE;
 
   return EXIT_SUCCESS;
 }
