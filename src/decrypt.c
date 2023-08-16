560913d21567b11f889a05494309264b256673fc
Henry Till <henrytill@gmail.com>
2023-08-15 17:38:13 -0700

    update .clang-format and reformat

--- src/decrypt.c
+++ src/decrypt.c
@@ -3,7 +3,6 @@
 #include "cipher.h"
 #include "data.h"
 
-int main(void)
-{
-	return cipher_decrypt(FINGERPRINT, stdin, stdout, NULL);
+int main(void) {
+  return cipher_decrypt(FINGERPRINT, stdin, stdout, NULL);
 }
